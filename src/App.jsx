d413a144557cc7a37e0a94eee7089ebab13cb4e2
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-01 23:35:37 -0300

    project start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { GlobalStyle } from './GlobalStyle'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <p>Start</p>
+        <GlobalStyle />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Inder&display=swap');
+
+    :root {
+        --maxWidth: 1280px;
+        --clr-white: #ffffff;
+        --clr-light-gray: #f4f4f5;
+        --clr-medium-gray: #d4d4d8;
+        --clr-dark-gray: #18181b;
+        --font-xl: 2.5rem;
+        --font-lg: 1.5rem;
+        --font-md: 1.2rem;
+        --font-sm: 1rem;
+    }
+
+    * {
+        box-sizing: border-box;
+        font-family: 'Inder', sans-serif;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    h1 {
+        font-size: 2rem;
+        font-weight: 600;
+        color: var(--clr-white)
+    }
+
+    h3 {
+        font-size: 1.1rem;
+        font-weight: 600;
+    }
+
+    p {
+        font-size: 1rem;
+        color: var(--white);
+    }
+
+`
