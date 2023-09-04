182b20d379de453358e8db1c1f5db757ab3b4ee7
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-03 22:31:48 -0300

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 //components
 import Header from './components/Header'
-// import Home from './components/Home'
+import Home from './components/Home'
 
 //styles
 import { GlobalStyle } from './GlobalStyle'
@@ -12,10 +12,9 @@ function App() {
   return (
     <>
       <div className="App">
-        <Header/>
-        {/* <Home/> */}
-        <p>Start</p>
         <GlobalStyle />
+        <Header/>
+        <Home/>
       </div>
     </>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 //configs
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config.js'
 
 
 //components
+import HeroImage from './HeroImage'
 
 
 //hooks
-
+import { useHomeFetch } from '../hooks/useHomeFetch.jsx'
 
 
 //images
@@ -16,15 +18,20 @@ import NoImage from '../images/no-image.jpg'
 
 
 const Home = () => {
-    
-    const [state, setState] = useState()
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
-    
+
+    const { state, loading, error} = useHomeFetch() 
+
+    console.log(state)
+
     return (
-        <div>
-            home page
-        </div>
+        <>
+        {state.results[5] && <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[5].backdrop_path}`}
+        title={state.results[5].original_title}
+        text={state.results[5].overview}
+        />}
+            
+        </>
     )
 }
 
