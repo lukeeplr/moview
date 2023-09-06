8c8c49da5a4c8a9f60690712a66ee615c4fb4c21
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-05 21:00:41 -0300

    add loader, grid, thumb and search to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 
 //configs
@@ -7,6 +7,10 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config.js'
 
 //components
 import HeroImage from './HeroImage'
+import Grid from './Grid'
+import Thumbnail from './Thumbnail'
+import Loader from './Loader.jsx'
+import SearchBar from './SearchBar/index.jsx'
 
 
 //hooks
@@ -19,18 +23,31 @@ import NoImage from '../images/no-image.jpg'
 
 const Home = () => {
 
-    const { state, loading, error} = useHomeFetch() 
-
-    console.log(state)
+    const { state, loading, error, searchTerm, setSearchTerm} = useHomeFetch() 
 
     return (
         <>
-        {state.results[5] && <HeroImage 
-        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[5].backdrop_path}`}
-        title={state.results[5].original_title}
-        text={state.results[5].overview}
+        {state.results[0] && <HeroImage 
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+        title={state.results[0].original_title}
+        text={state.results[0].overview}
         />}
-            
+        <SearchBar setSearchTerm={setSearchTerm}/>
+         <Grid header={searchTerm ? 'Results' : 'Popular Movies'}>
+            {state.results.map(movie => (
+                <Thumbnail 
+                key={movie.id}
+                image={
+                    movie.poster_path
+                    ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                    : NoImage
+                }
+                movieID={movie.id}
+                clickable={true}
+                 />
+                )
+            )}
+            </Grid>  
         </>
     )
 }
