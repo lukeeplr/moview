55f078de0911de14f22c0340807953e369e67c88
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-07 20:57:00 -0300

    create movie info container

---
+++ src/components/MovieInfo/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+//components
+import Thumbnail from '../Thumbnail'
+
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
+
+//image
+import NoImage from '../../images/no-image.jpg'
+
+//styles
+import { Wrapper, Content, Text } from './styles'
+
+const MovieInfo = ({ movie }) => {
+
+    return (
+    <Wrapper backdrop={movie.backdrop_path}>
+        <Content>
+            <Thumbnail image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage} clickable={false} />
+            <Text>
+                <h1><b></b>{movie.title}</h1>
+                <p><b>Sinopse: </b>{movie.overview}</p>
+                <p><b>Nota: </b>{movie.vote_average}</p>
+                <p><b>Data de lançamento: </b>{movie.release_date}</p>
+                <p><b>Direção: </b>{movie.directors.map(director => director.name).join(', ')}</p>
+            </Text>
+        </Content>
+    </Wrapper>
+    ) 
+}
+
+
+export default MovieInfo
---
+++ src/components/MovieInfo/styles.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from "../../config";
+
+export const Wrapper = styled.div`
+    background: ${({ backdrop }) => 
+    backdrop ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop}')` : '#000'
+};
+    background-size: cover;
+    background-position: center;
+    padding: 40px 20px;
+    animation: animateMovieInfo 1s;
+
+    @keyframes animateMovieInfo {
+        from {
+            opacity: 0; 
+        }
+        to {
+            opacity: 1;
+        }
+    }
+`
+
+export const Content = styled.div`
+    display: flex;
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 20px;
+
+    @media (max-width: 768px) {
+        display: block;
+        max-height: none;
+    }
+`
+
+export const Text = styled.div`
+    width: 100%;
+    padding: 20px 40px;
+    color: var(--clr-white);
+    overflow: hidden;
+
+    h1 {
+        font-size: var(--font-xl);
+
+        @media (max-width: 768px) {
+            font-size: var(--font-md);
+    }
+    }
+
+    p {
+        font-size: var(--font-lg);
+
+        @media (max-width: 768px) {
+            font-size: var(--font-sm);
+    }
+    }
+`
