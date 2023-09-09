b55b59e00d969fb881480e0b01a0db7535cac045
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-08 21:51:09 -0300

    actor style changes

--- src/components/Actor/styles.jsx
+++ src/components/Actor/styles.jsx
@@ -1,18 +1,19 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    color: var(--clr-light-gray);
-    background: var(--clr-dark-gray);
-    border-radius: 20px;
     padding: 5px;
     text-align: center;
 
     h3 {
         margin: 10px 0 0 0;
+        color: var(--clr-dark-gray);
+        font-size: var(--font-md);
     }
 
     p {
         margin: 5px 0;
+        color: #3F3F46;
+        font-size: var(--font-sm)
     }
 `
 
@@ -22,4 +23,5 @@ export const Image = styled.img`
     object-fit: cover;
     height: 200px;
     border-radius: 15px;
+    box-shadow: 8px -5px 8px rgba(0, 0, 0, 0.2);
 `
