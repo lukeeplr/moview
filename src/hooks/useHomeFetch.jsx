b73a3b5adc5c96bf7546ac413b975cd89c9c376d
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-09 18:13:27 -0300

    saves home fetch at session storage

--- src/hooks/useHomeFetch.jsx
+++ src/hooks/useHomeFetch.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 //api
 import API from '../API'
 
+import { isPersistedState } from '../helper'
+
 const initialState = {
     page: 0,
     results: [],
@@ -39,6 +41,15 @@ export const useHomeFetch = () => {
     }
 
     useEffect( () => {
+
+        if (!searchTerm) {
+            const sessionState = isPersistedState('homeState')
+            if (sessionState) {
+                setState(sessionState)
+                return
+            }
+        }
+
         setState(initialState)
         fetchMovies(1, searchTerm)
     }, [searchTerm])
@@ -49,6 +60,13 @@ export const useHomeFetch = () => {
         fetchMovies(state.page + 1, searchTerm)
         setIsLoadingMore(false)
     },[isLoadingMore, searchTerm, state.page])
+
+    useEffect( () => {
+        if (!searchTerm && state.page != 0) {
+            sessionStorage.setItem('homeState', JSON.stringify(state))
+        }
+    }, [searchTerm, state])
+
 
     return { state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore}
 
